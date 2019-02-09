7b9a53861c7af232193fc754b42aa161310654c8
Vinícius Machado <viniciusfm1@outlook.com>
2019-02-09 01:07:12 -0200

    standardizing documentation

--- bitcointrade.py
+++ bitcointrade.py
@@ -28,65 +28,63 @@ class Bitcointrade:
         response.close()
         return response.json()
 
-    def ticker(self):
+    def ticker(self, param1):
         """Retorna informações com o resumo das últimas 24 horas de negociações."""
         command = 'ticker'
         return self.get(command)
 
     def orders(self):
-        """Retorna lista de ordens ativas atualmente no book de ofertas"""
+        """Retorna lista de ordens ativas atualmente no book de ofertas."""
         command = 'orders'
         return self.get(command)
 
     def trades(self, start_time, end_time, page_size):
-        """Retorna lista de trades baseados nos critérios de pesquisa"""
+        """Retorna lista de trades baseados nos critérios de pesquisa."""
         command = 'trades?start_time={start_time}&end_time={end_time}&page_size={page_size}&current_page=1'.format(
             start_time = start_time, end_time = end_time, page_size = page_size
         )
         return self.get(command)
 
     def balance(self):
-        """Lista de carteiras e saldos"""
+        """Lista de carteiras e saldos."""
 
         response = requests.get(self.privateUrl + '/wallets/balance', headers = self.headers)
         return response.json()
 
     def book_orders(self):
-        """Retorna informações das ordens de compra e venda e executadas 
-        do livro de ofertas de uma determinada moeda"""
+        """Retorna informações das ordens de compra e venda e executadas do livro de ofertas de uma determinada moeda."""
 
         response = requests.get(self.privateUrl + '/market?pair={pair}'.format(pair = self.market),headers = self.headers)
         return response.json() 
 
     def summary(self):
-        """Retorna o resumo de uma moeda nas últimas 24 horas"""
+        """Retorna o resumo de uma moeda nas últimas 24 horas."""
 
         response = requests.get(self.privateUrl + '/market/summary?pair={pair}'.format(self.market), headers = self.headers)
         return response.json()
 
     def estimate(self, amount, typeorder):
-        
-        """Retorna o preço estimado de uma determinada quantidade de moeda"""
+        """Retorna o preço estimado de uma determinada quantidade de moeda."""
 
         response = requests.get(self.privateUrl + '/market/estimated_price?amount={amount}&pair={pair}&type={typeorder}'.format(
             amount = amount, pair = self.market, typeorder = typeorder), headers = self.headers)
         return response.json()
 
     def create_order(self, command, amount, price):
-        """Cria uma ordem de compra ou venda"""
+        """Cria uma ordem de compra ou venda."""
 
         data = {'pair': self.market, 'type': command, 'subtype': 'limited', 'amount': amount, 'unit_price': price, 'request_price': amount * price }
         response = requests.post(self.privateUrl + '/market/create_order', data = data, headers = self.headers)
         return response.json()
 
     def user_orders(self, status, start_date, end_date, pair, type_order):
-        """Retorna informações das ordens de compra e venda do usuário"""
+        """Retorna informações das ordens de compra e venda do usuário."""
         
         response = requests.get(self.privateUrl + '/market/user_orders/list?status={status}&start_date={start_date}&end_date={end_date}&pair={pair}&type={type}&page_size=50'.format(status = status, start_date = start_date, end_date = end_date, pair = self.market, type = type_order), headers = self.headers)
         return response.json()
 
     def cancel_order(self, orderId):
-        """Cancela uma ordem de compra ou venda"""
+        """Cancela uma ordem de compra ou venda."""
         
         data = {'id': orderId}
         response = requests.delete(self.privateUrl + '/market/user_orders', data = data, headers = self.headers)
