a51ab49a26f24880a6e7e7e0b8620c091672cbe9
Vinícius Machado <viniciusfm1@outlook.com>
2019-06-09 15:07:16 -0300

    test

--- testes.py
+++ testes.py
@@ -1,40 +1,11 @@
 from bitcointrade import Bitcointrade
-import datetime
 
-versao ='v2'
-moeda ='LTC'
-par = 'BRL' + moeda
-exchange = Bitcointrade(versao,par)
+exchange = Bitcointrade('brlltc','apitoken')
 
-ticker = exchange.ticker()
-orders = exchange.orders()
-trades = exchange.trades()
+book_orders = exchange.book_orders()
 
-if ticker['message'] == None:
-    print('# TICKER #------------------------------')
-    ticker = ticker['data']
-    print('última negociação :',ticker['last'])
-    print('volume 24h        :',ticker['volume'])
-    print('compra            :',ticker['buy'])
-    print('venda             :',ticker['sell'])
+print(book_orders)
 
-if orders['message'] == None:
-    print('# ORDERS #------------------------------')
-    orders = orders['data']
-    bids = orders['bids']
-    asks = orders['asks']
 
-    for i in range(5):
-        print(bids[i])
-
-print('# TRADES #------------------------------')
-if trades['message'] == None:
-    trades = trades['data']['trades']
-    print('mostrar execuções de compra')
-    for trade in trades:
-        if trade['type'] == 'buy':
-            print(trade['amount'],'@',trade['unit_price'])
-            print(trade['date'])
-            print('______________________')
 
 
